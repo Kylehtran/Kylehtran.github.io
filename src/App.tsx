9bd99a1efdbe5a5caa9ca201ce73d75553342ee6
Kyle Tran <trankyle2002@yahoo.com>
2024-12-19 00:57:29 -0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './Home'; // Import the HomePage component
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} /> 
-                </Routes>
-        </Router>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
-    
